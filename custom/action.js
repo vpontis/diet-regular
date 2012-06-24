32a6d8d514a44a882bae90ace87206df4dfa0233
Victor Pontis <vpontis@gmail.com>
2012-06-24 17:17:47 -0400

    All but the graphing function

--- custom/action.js
+++ custom/action.js
@@ -1,39 +1,4 @@
 function makeGraph(){
-    // Check for the various File API support.
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-        // Great success! All the File APIs are supported.
-    } else {
-        alert('The File APIs are not fully supported in this browser.');
-    }
-
-}
-
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "data.txt",
-        dataType: "text",
-        success: function(data) {
-            processData(data);
-        }
-    });
-});
-
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);  
-        }
-    }
-    // alert(lines);
+    data = load('data.csv');
+    $('#insert-graph').html(data);
 }
